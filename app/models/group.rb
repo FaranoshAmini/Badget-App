7cf103505701727048566f8aa9d77442a388e165
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-16 11:50:08 +0430

    Add relationship between models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
     validates :Name, presence: true
     validates :Icon, presence: true
     belongs_to :user
+    has_many :transacts
 end
--- app/models/transact.rb
+++ app/models/transact.rb
@@ -3,4 +3,5 @@ class Transact < ApplicationRecord
   validates :Amount, presence: true
   validates :Amount, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :authorid
+  has_many :groups
 end
