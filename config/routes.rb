6b17f4548b339ac7bc8a632f314ee7ebb4a928f8
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-17 14:50:18 +0430

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    authenticated :user do
+      root 'groups#index', as :authenticated_root
+    end
+    resources :groups, only: [:index, :new, :create] do
+      resources :expenses, only: [:index, :new, :create] 
+    end
 end
