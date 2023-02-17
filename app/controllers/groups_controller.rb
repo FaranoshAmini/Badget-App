91bfab7a0947bfd45e086bdd8ac7467b0576e2c3
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-17 15:04:47 +0430

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,26 @@
 class GroupsController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @groups = current_user.groups
-    end
-    
-    def new
-        @group = Group.new
-    end
-    
-    def create
-        @group = current_user.groups.new(cate_params)
-        if @group.save
-            flash[:notice] = "Successfully created group."
-            redirect_to groups_path
-        else
-            flash[:notice] = "Invalid group. Please try again."
-        end
-    end
-    
-    private
+  before_action :authenticate_user!
+  def index
+    @groups = current_user.groups
+  end
 
-    def cate_params
-        params.require(:group).permit(:name)
-    end    
-end 
-  
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.new(gro_params)
+    if @group.save
+      flash[:notice] = 'Successfully created group.'
+      redirect_to groups_path
+    else
+      flash[:notice] = 'Invalid group. Please try again.'
+    end
+  end
+
+  private
+
+  def gro_params
+    params.require(:group).permit(:name)
+  end
+end
